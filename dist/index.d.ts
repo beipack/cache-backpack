20affab128c31cc81a48cee3f96c515d20b0a25b
Phan Shi Yu <phanshiyu@gmail.com>
2022-07-29 17:46:33 +0800

    fix: prepublish script

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-interface FunctionWithBackpack<T extends (...args: any) => Promise<any>> {
-    functionWithBackpack: (...args: Parameters<T>) => Promise<Awaited<T>>;
-    emptyBackpack: () => void;
-}
-export declare function carryBackpack<T extends (...args: any[]) => Promise<any>>(fn: T, getLatestItemVersion: () => string, initialVersion?: string, makeNoise?: boolean): FunctionWithBackpack<T>;
-export {};
